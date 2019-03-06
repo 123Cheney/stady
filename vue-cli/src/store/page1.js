ae0c197eaf264e63ce44db18188743e8cd366d86
chenqiong <362120005@qq.com>
2019-03-06 17:05:45 +0800

    test

--- vue-cli/src/store/page1.js
+++ vue-cli/src/store/page1.js
@@ -12,6 +12,7 @@ const page1={
   },
   // action 的作用跟mutation的作用是一致的，它提交mutation，从而改变state，是改变state的一个增强版
   // 不同在于：Action 提交的是 mutation，而不是直接变更状态
+  
   actions:{
     changeInfo({commit},params){
       commit('forInfo',params)
