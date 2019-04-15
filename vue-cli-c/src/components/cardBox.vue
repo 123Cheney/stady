990640f2d95df1b6104b951d81d1bb155633de4c
chenqiong <362120005@qq.com>
2019-04-15 17:36:51 +0800

    自适应盒子

---
+++ vue-cli-c/src/components/cardBox.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="card-box" ref="cardBox">
+    <div class="card-item" v-for="(item,index) in cardData" 
+      :key="index" 
+      :style="itemSty">
+      <slot :data="item"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  props: {
+    itemSty: {
+      type: Object,
+      default() {
+        return {
+          width:'250px',
+          height:'280px',
+          background: '#aaa',
+          marginBottom: '20px',
+        }
+      },
+    },
+    cardData:{
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  mounted() {
+    let _this = this;
+    window.onresize=function(){
+      _this.boxGrid();
+    }
+    this.boxGrid()
+  },
+  methods: {
+    boxGrid() {
+      let cardBox = this.$refs.cardBox;
+      let cardItem = document.getElementsByClassName("card-item");
+      let [boxWidth, cardWidth, num, marginRight] = ["", "", "", ""];
+
+      if (cardItem[0] && cardBox != undefined) {
+        boxWidth = cardBox.clientWidth;
+        cardWidth = cardItem[0].clientWidth;
+        
+        num = parseInt(boxWidth/cardWidth);
+        marginRight = (boxWidth - (cardWidth * num)) / (num-1);
+        if(marginRight<=10){
+          num = num-1;
+          marginRight = (boxWidth - (cardWidth * num)) / (num-1);
+        }
+
+        for (let i = 0; i < cardItem.length; i++) {
+          if ((i + 1) % num != 0) {
+            cardItem[i].style.marginRight = parseInt(marginRight - 1) + 'px';
+          } else {
+            cardItem[i].style.marginRight = '0px';
+          }
+        }
+      }
+    }
+  },
+  watch: {},
+  computed: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.card-box{
+  display: flex;
+  flex-flow: row wrap;
+  .card-item{
+    box-sizing: border-box;
+    box-shadow: 0px 0px 8px 2px rgba(0, 0, 0, 0.15);
+  }
+}
+
+</style>
---
+++ vue-cli-c/src/view/page6.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="page">
+    <cardBox :cardData='cardData'>
+      <template slot-scope="val">
+        <h1>{{val.data.title}}</h1>
+      </template>
+    </cardBox>
+  </div>
+</template>
+
+<script>
+import cardBox from "../components/cardBox";
+
+export default {
+  components: { cardBox },
+  data() {
+    return {
+      cardData:[
+        {
+          title: '选项卡一',
+          content: '选项卡一的内容'
+        },
+        {
+          title: '选项卡二',
+          content: '选项卡二的内容'
+        },
+        {
+          title: '选项卡三',
+          content: '选项卡三的内容'
+        }
+      ]
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
