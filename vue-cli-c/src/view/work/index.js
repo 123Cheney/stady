e2e460400c98ef414f9d61e8869692fdcc2f13bd
chenqiong <362120005@qq.com>
2019-04-22 19:26:47 +0800

    upadat

---
+++ vue-cli-c/src/view/work/index.js
@@ -0,0 +1,15 @@
+var img1 = document.getElementById('click1');
+var img2 = document.getElementById('click2');
+var img3 = document.getElementById('click3');
+var img4 = document.getElementById('click4');
+
+
+img1.onclick=function(){
+  
+}
+
+
+// var orderDiv = document.createElement("div");
+// orderDiv.class="order-box";
+// document.getElementById("left").appendChild(orderDiv);                   
+// var radio= document.createElement("input");
