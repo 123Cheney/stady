73a0f659f3df028dd6c022bc5a1a6868190f5129
chenqiong <362120005@qq.com>
2019-04-23 17:18:24 +0800

    tija

--- vue-cli-c/src/view/work/index.js
+++ vue-cli-c/src/view/work/index.js
@@ -1,10 +1,7 @@
-var img1 = document.getElementById('click1');
+var img1 = document.getElementById('click1'); //可点击元素
 var img2 = document.getElementById('click2');
 var img3 = document.getElementById('click3');
 var img4 = document.getElementById('click4');
-
-
-
 
 var tableData = [
   {index:'1',name:'单个企业社会信用代码验证',status:'已同意',provide:'市场监督管理局',apply:'公安局',option:'查看'},
@@ -35,29 +32,23 @@ tableData.map((item, index)=>{
                 <span onclick="showDetail('${item.index}')">${item.option}</span>
               </p>`
 })
-var popupBox = document.getElementById('popup');
-var popupDetail = document.querySelector('.popup .side');
 
-img1.onclick=function(){
-  popupBox.style.display='initial';
-  document.getElementById('top-p').innerHTML = '资源申请部门排序';
-  document.getElementById('foot-p').innerHTML = '资源提供部门排行';
-  document.querySelector('.order-box').innerHTML = orderTemp;
-  document.querySelector('.order-box2').innerHTML = orderTemp;
-  document.querySelector('.tbody').innerHTML = tabelTemp;
-}
+var popupBox = document.getElementById('popup');            //大弹窗
+var popupDetail = document.querySelector('.popup .side');   //弹窗详情
+var orderBoxTop = document.querySelector('.order-box');     //上部排序盒子
+var topP = document.getElementById('top-p');                //上部排序盒子标题
+var orderBoxFoot = document.querySelector('.order-box2');   //下部排序盒子
+var footP = document.getElementById('foot-p');       //下部排序盒子标题
 
-img2.onclick=function(){
-  popupBox.style.display='initial';
-  document.getElementById('top-p').innerHTML = '待审核资源提供部门排序';
-  document.getElementById('foot-p').innerHTML = '已审核资源提供部门排行';
-}
+orderBoxTop.innerHTML = orderTemp;
+orderBoxFoot.innerHTML = orderTemp;
 
-img4.onclick=function(){
-  popupBox.style.display='initial';
-  document.getElementById('top-p').innerHTML = '已办结资源申请部门排序';
-  document.getElementById('foot-p').innerHTML = '已办结资源提供部门排序';
-}
+document.querySelector('.tbody').innerHTML = tabelTemp;
+
+img1.addEventListener('click',function(){showPopup(1)});
+img2.addEventListener('click',function(){showPopup(2)});
+img3.addEventListener('click',function(){showPopup(3)});
+img4.addEventListener('click',function(){showPopup(4)});
 
 document.getElementById('del1').onclick=function(){
   popupBox.style.display='none';
@@ -68,9 +59,57 @@ document.getElementById('del2').onclick=function(){
   popupDetail.style.display='none';
 }
 
+// 弹框
+function showPopup(item){
+  popupBox.style.display='initial';
+  orderBoxTop.style.display = 'initial';
+  orderBoxFoot.style.display = 'initial';
+  topP.style.display = 'inherit';
+  footP.style.display = 'inherit';
+  switch(item){
+    case 1:
+      topP.innerText = '资源申请部门排序';
+      footP.innerHTML = '资源提供部门排行';
+      break;
+    case 2:
+      topP.innerHTML = '待审核资源提供部门排序';
+      footP.innerHTML = '已审核资源提供部门排行';
+      break;
+    case 3:
+      orderBoxTop.style.display = 'none';
+      orderBoxFoot.style.display = 'none';
+      topP.style.display = 'none';
+      footP.style.display = 'none';
+      break;
+    case 4:
+      topP.innerHTML = '已办结资源申请部门排序';
+      footP.innerHTML = '已办结资源提供部门排序';
+      break;
+  }
+}
+
+// 详情弹框
 function showDetail(item){
   popupDetail.style.display='initial';
   let index = parseInt(item)-1;
   document.getElementById('depart1').innerText = tableData[index].provide;
   document.getElementById('depart2').innerText = tableData[index].apply;
+}
+
+// 数字动态增加
+var label = document.querySelectorAll('.right-sceond-content ul li label');
+for(let i=0;i<label.length;i++){
+  numAdd(label[i],parseInt(label[i].innerText));
+};
+function numAdd(dom,max){
+  let i=0;
+  let timer = setInterval(function(){
+    if(i>=max){
+      clearInterval(timer);
+      dom.innerText = max;
+      return
+    }
+    dom.innerText = i;
+    i=i+3134;
+  },50)
 }
